4a646a41b0ecebcddbe1262d840b783510b93fc7
Chabli Mohammed Yassine <mychabli@octo.com>
2019-10-08 22:46:00 +0100

    fixing general styles over the app

--- templates/header.php
+++ templates/header.php
@@ -4,13 +4,16 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" type="text/css" href="../css/style.css">
 
-    <title>Simple Database App</title>
+      <title>Simple Database App</title>
 
     <link rel="stylesheet" href="../css/style.css" />
   </head>
 
   <body>
-    <h1>Simple Database App</h1>
+  <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">Simple Database App</a>
+  </nav>
   </body>
 </html>
